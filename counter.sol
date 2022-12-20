da5557627aa9680d6ac566a5480f1ac3c3445644
Yasin Yumrutepe <88865849+yasinyumrutepe@users.noreply.github.com>
2022-12-20 22:59:10 +0300

    Update counter.sol

--- counter.sol
+++ counter.sol
@@ -1,11 +1,13 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract Count{
+//I created a public unassigned integer variable named counter
     uint  public count;
-
+    //Function that increases the counter value by 1
     function increases() external {
         count+=1;
     }
+    //Function that decrease the counter value by 1
     function decrease()external  {
          count-=1;
     }
